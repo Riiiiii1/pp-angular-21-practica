e03d5559cd9e4f7d8fa476ab0e68f6c8022a5dfa
david <desbskull@gmail.com>
2026-05-13 20:41:12 -0500

    foter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeader } from "./components/app-header/app-header";
-
+import { Hero } from './components/hero/hero';
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeader],
+  imports: [RouterOutlet, AppHeader, Hero,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
