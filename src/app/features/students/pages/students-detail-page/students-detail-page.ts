6ca1efde9d88d45534eb8d1b10a40ecf746f9c04
Your Name <you@example.com>
2026-05-14 12:33:05 -0500

    add: Clase de 14/5/2026

---
+++ src/app/features/students/pages/students-detail-page/students-detail-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-detail-page',
+  imports: [],
+  templateUrl: './students-detail-page.html',
+  styles: ``,
+})
+export class StudentsDetailPage {}
