8c746d9a5af8178305455235dcf0bc2dec5d3e2f
david <desbskull@gmail.com>
2026-05-27 20:51:21 -0500

    add: favorites

--- src/app/features/simpsons/pages/simpson-detail-page/simpson-detail-page.ts
+++ src/app/features/simpsons/pages/simpson-detail-page/simpson-detail-page.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject ,signal} from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { SimpsonsService } from '../../services/simpsons.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of, tap } from 'rxjs'; // ← estos faltaban
 import { SimpsonsCacheService } from '../../services/simpsons-cache.service';
+import { FavoritesService } from '../../../../core/services/favorites';
+import { AuthService } from '../../../../core/services/auth';
 @Component({
   selector: 'app-simpson-detail-page',
   imports: [RouterLink],
@@ -15,7 +17,9 @@ export class SimpsonDetailPage {
   private simpsonsService = inject(SimpsonsService);
   private cacheService = inject(SimpsonsCacheService);
   private characterId = Number(this.route.snapshot.paramMap.get('id'));
-
+  isFavorite = signal(false);
+  private favoritesService = inject(FavoritesService);
+  authService = inject(AuthService);
 
   characterResource = rxResource({
     stream: () => {
@@ -33,4 +37,21 @@ export class SimpsonDetailPage {
       );
     },
   });
+
+    toggleFavorite() {
+    const uid = this.authService.uid;
+    if (!uid) return; // No hace nada si no hay sesion activa.
+
+    if (this.isFavorite()) {
+      // Si ya es favorito, lo eliminamos de Firestore.
+      this.favoritesService.removeFavorite(uid, this.characterId).then(() => {
+        this.isFavorite.set(false);
+      });
+    } else {
+      // Si no es favorito, lo guardamos en Firestore.
+      this.favoritesService.addFavorite(uid, this.characterId).then(() => {
+        this.isFavorite.set(true);
+      });
+    }
+  }
 }
